1492203bd99c441cc66630267c4fe583962c7c53
bchansen88 <b.c.hansen88@gmail.com>
2018-04-26 12:43:49 -0400

    added code to create csv if not exists

--- pull-data.py
+++ pull-data.py
@@ -1,19 +1,24 @@
 import pandas as pd
+import os
+
 
 
 year = '2017'
 pos = 'RB'
-
+file = 'C:\\Users\\Ben\\PythonProjects\\football.analytics\\defense' + year + '.csv'
+    
 
 
 class DefenseFP:
-    
+
     def __init__(self, year, position):
         
         self.year = year
         self.position = position
         
     def getData(self):
+        
+
         url = 'https://www.pro-football-reference.com/years/' + year + \
               '/fantasy-points-against-' + pos + '.htm'
               
@@ -26,13 +31,24 @@ class DefenseFP:
         #sets the index to teams
         df.set_index(['Tm'])
         #Rename columns
-        df.columns = ['Tm', 'G', 'Att', 'Ru-Yds', 'Ru-TD', 'Tgt', 'Rec', 'Rec-yds', 
-              'Rec-TD', 'FantPt', 'DKPt', 'FDPt', 'FantPt', 'DKPt', 'DFPt']
-        df.to_csv('C:\\Users\\Ben\\env\\football.analytics\\defense' + \
-                  year + '.csv')
+        #df.columns = ['Tm', 'G', 'Att', 'Ru-Yds', 'Ru-TD', 'Tgt', 'Rec', 'Rec-yds', 
+        #      'Rec-TD', 'FantPt', 'DKPt', 'FDPt', 'FantPt', 'DKPt', 'DFPt']
+        
+        if not os.path.exists(file):
+            
+            df.to_csv(file)
+        
         return df
+    
+
+    
+        
+    
+    def saveToFile(self, df):
+        
+        df.to_csv(file)
         
     
         
 rb = DefenseFP(year, pos)
-print(rb.getData())
+print(rb.getData().head())

09e1c88e143bc431165900f3b9a2315ebd0d31a2
bchansen88 <b.c.hansen88@gmail.com>
2018-05-10 12:45:58 -0400

    added teams list, started to figure out plotting in test.py

--- test.py
+++ test.py
@@ -6,11 +6,34 @@ Created on Sat Apr 28 21:43:32 2018
 """
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 file = 'calcTest.csv'
+x = []
+y = []
 
 df = pd.read_csv(file)
+#df = df.set_index('Tm')
+df = df[['Tm', 'Rec']]
+print(df)
+
+
+#need to figure this one out
+df.plot(x = 'Tm', y = 'Rec', kind='scatter')
+
+
+'''
 df = df.set_index('Tm')
 
-print(df.head())
+forecast_col = 'Rec'
+
+#empty data treated as an outlier
+df.fillna(-99999, inplace = True)
+
+#predict 10% out of the data frame
+forecast_out = int(math.ceil(0.1 * len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+print(df.tail())
+'''
